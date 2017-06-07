0bf9b50fcde2a40c0c2bf590082a39eb1d0a13f2
AvramBogdan <@mail>
2017-06-07 11:01:31 +0300

    Automated commit from OIDEA.

--- pp.c
+++ pp.c
@@ -1,10 +1,16 @@
 #include<stdio.h>
 int main()
 { 
-  float y = 0.0;
-  printf("am schimbat pentru git");
-  return 0; 
+  int a[10] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+  int max = 0 ;
+  int i;
   
+  for (i = 0; i <= 10; i++)
+    if (max < a[i])
+      max = i;
+  
+  printf("maximul este = %d", max);
+  return 0;
 }
 
   
